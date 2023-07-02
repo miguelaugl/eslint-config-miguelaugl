0b873808595b041e741c8698acd44d3019a2e836
Miguel <miguelaugl@outlook.com>
2023-07-02 19:57:27 -0300

    feat: eslint config for react

---
+++ README.md
@@ -0,0 +1,27 @@
+# eslint-config-miguelaugl
+This is my eslint configuration for React
+
+[![CircleCI](https://circleci.com/gh/wcandillon/eslint-config-react-native-wcandillon.svg?style=svg)](https://circleci.com/gh/wcandillon/eslint-config-react-native-wcandillon)
+[![npm version](https://badge.fury.io/js/eslint-config-miguelaugl.svg)](https://badge.fury.io/js/eslint-config-miguelaugl)
+
+## Usage
+
+```sh
+# you also need eslint if not installed already: yarn add eslint --dev
+yarn add eslint-config-miguelaugl --dev
+
+or
+
+# you also need eslint if not installed already: npm install eslint --save-dev
+npm install eslint-config-miguelaugl --save-dev
+```
+
+## React
+
+In `.eslintrc`:
+
+```json
+{ 
+  "extends": "eslint-config-miguelaugl/react", 
+} 
+```
---
+++ react.js
@@ -0,0 +1,91 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'standard',
+    'plugin:prettier/recommended',
+    'plugin:import/typescript',
+  ],
+  plugins: [
+    'react',
+    'jsx-a11y',
+    '@typescript-eslint',
+    'import',
+    'unused-imports',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+        semi: false,
+        endOfLine: 'auto',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'jsx-a11y/alt-text': [
+      'warn',
+      {
+        elements: ['img'],
+        img: ['Image'],
+      },
+    ],
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling', 'index'],
+          'type',
+          'object',
+        ],
+        'newlines-between': 'always',
+        pathGroups: [{ pattern: 'react', group: 'external' }],
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    },
+  },
+}
